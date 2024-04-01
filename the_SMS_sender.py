217513654b623596ce26d8ce4e201309e44fd338
Eric-chagas <eric_chagas12@outlook.com>
2024-04-01 19:13:13 -0300

    fixing dotenv missing library and messages

--- the_SMS_sender.py
+++ the_SMS_sender.py
@@ -1,10 +1,15 @@
 # Importing libraries and twilio-python important stuff
 import os
+from dotenv import load_dotenv
 from twilio.rest import Client
 
+
+# Loading .env file 
+load_dotenv()
+
 # Setting up twilio account stuff located in very secret, obviously .gitIgnored .env config file
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
 # Finally creating client connection
 client = Client(account_sid, auth_token)
@@ -12,9 +17,9 @@ client = Client(account_sid, auth_token)
 # Write your bullshit message bellow
 
 message = client.messages.create(
-    body="Hi! My name is Eric, i'm a CNU nerd! Come join my nerdy bullshit at CNU, virginia!",
-    from_=os.environ['SENDER_PHONE_NUMBER'],
-    to=os.environ['RECEIVER_PHONE_NUMBER_BR']
+    body="Hi! My name is Eric, i'm a CNU nerd! Come join my nerdy troop at mojo dojo casa house, CNU, Virginia!",
+    from_=os.getenv("SENDER_PHONE_NUMBER"),
+    to=os.getenv("RECEIVER_PHONE_NUMBER_US")
 )
 
 # Regular python debug stuff
